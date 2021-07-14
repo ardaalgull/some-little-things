1155eacf77c5a654ce46e29133949b458b7b5023
Arda Algül <80629081+ardaalgull@users.noreply.github.com>
2021-07-14 06:12:12 +0300

    Add files via upload

---
+++ FPSGameWithoutDemos/FPSGameWithoutDemos.cpp
@@ -0,0 +1,87 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayerAngle = 0.0f;
+float fFOV = 3.14159f / 4.0f;
+float fDepth = 16.0f;
+float fSpeed = 5.0f;
+
+
+
+int main()
+{
+	// Screen Buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	wstring map;
+
+
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+	while (1) // Game loop here
+	{
+		for (int x = 0; x < nScreenWidth; x++) 
+		{
+			float fRayAngle = (fPlayerAngle - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistance2Wall = 0;
+			bool bHitWall = false;  
+
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall) 
+			{
+
+
+				fDistance2Wall += 0.1f;
+			}
+		}
+
+
+
+
+
+
+
+		
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+
+
+
+	return 0;
+}
